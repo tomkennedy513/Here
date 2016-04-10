28a85e79ad67bdf8484a292eb6f38fb85e9e7459
Tom Kennedy <tkenned8@villanova.edu>
2016-04-10 13:15:08 -0400

    something with gradle

--- app/src/main/java/edu/villanova/tkenned8/here/MainActivity.java
+++ app/src/main/java/edu/villanova/tkenned8/here/MainActivity.java
@@ -1,8 +1,7 @@
 package edu.villanova.tkenned8.here;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent map = new Intent(this, MapsActivity.class);
-        startActivity(map);
+
     }
 }
