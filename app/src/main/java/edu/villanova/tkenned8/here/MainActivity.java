89e0ca38c86685dad54907ded3447b989838ecfe
Tom Kennedy <tkenned8@villanova.edu>
2016-04-13 14:17:44 -0400

    did some stuff

--- app/src/main/java/edu/villanova/tkenned8/here/MainActivity.java
+++ app/src/main/java/edu/villanova/tkenned8/here/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.villanova.tkenned8.here;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button mapButton = (Button) findViewById(R.id.mapButton);
+        assert mapButton != null;
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button addPinButton = (Button) findViewById(R.id.locationSearchButton);
+        assert addPinButton != null;
+        addPinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, locationSearchActivity.class);
+                startActivityForResult(intent2, 1);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/villanova/tkenned8/here/locationSearchActivity.java
@@ -0,0 +1,36 @@
+package edu.villanova.tkenned8.here;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+
+public class locationSearchActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
+    LatLng newLatLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_search);            PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.getName());
+                newLatLng = place.getLatLng();
+
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+}
+}
