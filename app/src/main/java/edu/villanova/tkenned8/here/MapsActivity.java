6a193ffc93c1d5b63d7b05310495e65152ea9795
Tom Kennedy <tkenned8@villanova.edu>
2016-04-24 20:33:56 -0400

    4/24 sitting at library table may have fixed maps

--- app/src/main/java/edu/villanova/tkenned8/here/MapsActivity.java
+++ app/src/main/java/edu/villanova/tkenned8/here/MapsActivity.java
@@ -37,7 +37,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
         ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
