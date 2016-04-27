b0aadec3d82712c24cf3ff58660865d14ebd3050
Tom Kennedy <tomkennedy513@users.noreply.github.com>
2016-04-27 02:01:56 -0400

    texting function is passed current distance in miles

--- app/src/main/java/edu/villanova/tkenned8/here/locationNotifications.java
+++ app/src/main/java/edu/villanova/tkenned8/here/locationNotifications.java
@@ -1,20 +1,28 @@
 package edu.villanova.tkenned8.here;
 
+import android.Manifest;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
 
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by wildcat on 4/24/2016.
  */
-public class locationNotifications extends Service{
+public class locationNotifications extends Service {
     SmsManager mgr = null;
     private final IBinder binder = new MyBinder();
     String message;
@@ -24,120 +32,141 @@ public class locationNotifications extends Service{
     String contactName;
     String phoneNumber;
     String destinationType;
-    LatLng destination;
-
+    LatLng destinationCoord;
+    LocationListener listener;
+    Location destination;
     @Override
     public void onCreate() {
         super.onCreate();
 
         //Set up SMS manager
         mgr = SmsManager.getDefault();
-        Log.d("MyApp","Got to onCreate in service");
+        Log.d("MyApp", "Got to onCreate in service");
 
     }
 
     //needed to receive intent extras
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
+    public int onStartCommand(Intent intent, int flags, int startId) {
         //Retrieve intent information
         contactName = intent.getStringExtra("contactName");
         phoneNumber = intent.getStringExtra("phoneNumber");
         destinationType = intent.getStringExtra("destinationType");
         Bundle bundle = intent.getParcelableExtra("destination");
-        destination = bundle.getParcelable("destination");
+        destinationCoord = bundle.getParcelable("destination");
+        assert destinationCoord != null;
+        double lat = destinationCoord.latitude;
+        double lon =destinationCoord.longitude;
+        destination.setLatitude(lat);
+        destination.setLongitude(lon);
 
 
-        Log.d("MyApp","Contactname = " + contactName + " Phonenumber = " + phoneNumber + " destinationType = " + destinationType);
+
+        Log.d("MyApp", "Contactname = " + contactName + " Phonenumber = " + phoneNumber + " destinationType = " + destinationType);
 
         //get Settings from Shared Preferences
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        message = sharedPreferences.getString("text",null);
-        oneMile = sharedPreferences.getBoolean("one",false);
+        message = sharedPreferences.getString("text", null);
+        oneMile = sharedPreferences.getBoolean("one", false);
         fiveMile = sharedPreferences.getBoolean("five", false);
         tenMile = sharedPreferences.getBoolean("ten", false);
 
-        Log.d("MyApp","Got to onStartCommand in service, message = " + message + " " + oneMile.toString() + " " + fiveMile.toString() + " " + tenMile.toString());
-
-        //Logic for messages being set & different notifcation idstances
-        if(message == "")
-        {
-            Log.d("MyApp","No message to display");
-            if(destinationType.equals("arriving"))
-            {
-                Log.d("MyApp","reached arrive clause");
-                mgr.sendTextMessage(phoneNumber,null,contactName + " has left",null,null);
-                if(tenMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,contactName + " is ten miles away from their destination", null,null);
-                }
-                else if (fiveMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,contactName + " is five miles away from their destination", null,null);
-                }
-                else if (oneMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,contactName + " is one mile away from their destination", null,null);
-                }
-
-            }
-            else {
-                Log.d("MyApp","reached pickingUp clause");
-                mgr.sendTextMessage(phoneNumber,null,"Someone is leaving to pick you up", null,null);
-                if(tenMile == true) //& location range is correct
-                {
-                    mgr.sendTextMessage(phoneNumber,null,"Someone is ten miles away", null,null);
-                }
-                else if (fiveMile == true)//& location range is correct
-                {
-                    mgr.sendTextMessage(phoneNumber,null,"Someone is five miles away", null,null);
-                }
-                else if (oneMile == true)//& location range is correct
-                {
-                    mgr.sendTextMessage(phoneNumber,null,"Someone is one mile away", null,null);
-                }
-                //else {} //location arrive  mgr.sendTextMessage(phoneNumber,null,contactName + " has arrived", null,null);
-            }
-
+        Log.d("MyApp", "Got to onStartCommand in service, message = " + message + " " + oneMile.toString() + " " + fiveMile.toString() + " " + tenMile.toString());
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         }
-        else {
-            if(destinationType.equals("arriving"))
-            {
-                Log.d("MyApp","reached arrive clause");
-                mgr.sendTextMessage(phoneNumber,null,message,null,null);
-                if(tenMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,message, null,null);
-                }
-                else if (fiveMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,message, null,null);
-                }
-                else if (oneMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,message, null,null);
-                }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, listener);
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                float distanceInMeters= location.distanceTo(destination);
+                float distanceInMiles = (float) (distanceInMeters/1609.344);
+                locationCheck(distanceInMiles);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
             }
-            else {
-                Log.d("MyApp","reached pickingUp clause");
-                if(tenMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,message, null,null);
-                }
-                else if (fiveMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,message, null,null);
-                }
-                else if (oneMile == true)
-                {
-                    mgr.sendTextMessage(phoneNumber,null,message, null,null);
-                }
-            }
-        }
 
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
         return START_STICKY;
     }
+
+        public void locationCheck(float distanceInMiles) {
+
+            if (message == "") {
+                Log.d("MyApp", "No message to display");
+                if (destinationType.equals("arriving")) {
+                    Log.d("MyApp", "reached arrive clause");
+                    mgr.sendTextMessage(phoneNumber, null, contactName + " has left", null, null);
+                    if (tenMile) {
+                        mgr.sendTextMessage(phoneNumber, null, contactName + " is ten miles away from their destination", null, null);
+                    } else if (fiveMile) {
+                        mgr.sendTextMessage(phoneNumber, null, contactName + " is five miles away from their destination", null, null);
+                    } else if (oneMile) {
+                        mgr.sendTextMessage(phoneNumber, null, contactName + " is one mile away from their destination", null, null);
+                    }
+
+                } else {
+                    Log.d("MyApp", "reached pickingUp clause");
+                    mgr.sendTextMessage(phoneNumber, null, "Someone is leaving to pick you up", null, null);
+                    if (tenMile) //& location range is correct
+                    {
+                        mgr.sendTextMessage(phoneNumber, null, "Someone is ten miles away", null, null);
+                    } else if (fiveMile)//& location range is correct
+                    {
+                        mgr.sendTextMessage(phoneNumber, null, "Someone is five miles away", null, null);
+                    } else if (oneMile)//& location range is correct
+                    {
+                        mgr.sendTextMessage(phoneNumber, null, "Someone is one mile away", null, null);
+                    }
+                    //else {} //location arrive  mgr.sendTextMessage(phoneNumber,null,contactName + " has arrived", null,null);
+                }
+
+            } else {
+                if (destinationType.equals("arriving")) {
+                    Log.d("MyApp", "reached arrive clause");
+                    mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                    if (tenMile) {
+                        mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                    } else if (fiveMile) {
+                        mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                    } else {
+                        if (oneMile) {
+                            mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                        }
+                    }
+
+                } else {
+                    Log.d("MyApp", "reached pickingUp clause");
+                    if (tenMile) {
+                        mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                    } else if (fiveMile) {
+                        mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                    } else if (oneMile) {
+                        mgr.sendTextMessage(phoneNumber, null, message, null, null);
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
 
     public class MyBinder extends Binder {
         //return an instance of the service
